c06a3fe26261daa5e2c54896abeade85ff5b8556
Sebastian Duran <sebastian.duran@alumnos.uach.cl>
2020-04-12 16:12:55 -0400

    funcionalidad de botones

--- pages/cubiculo/[id].tsx
+++ pages/cubiculo/[id].tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import { Button, Card, CardGroup } from "react-bootstrap";
+import axios from "axios";
 
+import { useEffect, useState } from "react";
 import { Cubiculo } from "../../src/interfaces";
 //import {onClickChange} from "../miraflores";
 
@@ -12,14 +14,61 @@ export default () => {
   const cubiculoId = id ?.toString() ?? "";
   //const cubiculoDisponibilidad = index ?.toString() ?? "";
 
+  // para estableser el estado de cubiculo
+  const [cubiculosState, setCubiculosState] = useState<Cubiculo[]>([]);
+
+  const onClickChange = (id: string, disponibilidad: 0 | 1 | 2) => {
+    setCubiculosState(
+      cubiculosState.map(cubiculo => {
+        if (cubiculo._id === id) {
+          cubiculo.disponibilidad = disponibilidad;
+        }
+        return cubiculo;
+      })
+    );
+  };
+
+  const actualizarCubiculo = async (
+    cubiculoId: string,
+    disponibilidad: 0 | 1 | 2
+  ) => {
+    axios
+      .post("/api/editCubiculoDisponibilidad", {
+        cubiculo: cubiculoId,
+        disponibilidad
+      })
+      .then(response => {
+        const cubiculoData = response.data;
+        setCubiculosState(
+          cubiculosState.map(cubiculo => {
+            if (cubiculo._id === cubiculoData._id) {
+              cubiculo.disponibilidad = disponibilidad;
+            }
+            return cubiculo;
+          })
+        );
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    axios.get("/api/cubiculos?project=miraflores").then(response => {
+      setCubiculosState(response.data);
+    });
+  }, []);
+
   return (
     <div>
       <CardGroup>
         <Card>
-          <Button>Habilitar Cubiculo {cubiculoId}</Button>
+          <button className="btn btn-light but" onClick={() => onClickChange(cubiculoId, 0)}>
+            Habilitar Cubiculo {cubiculoId}</button>
         </Card>
         <Card>
-          <Button>Deshabilitar {cubiculoId}</Button>
+          <button className="btn btn-light but" onClick={() => onClickChange(cubiculoId, 1)}>
+            Deshabilitar {cubiculoId}</button>
         </Card>
       </CardGroup>
       <hr className="featurette-divider" />
